740a49bd645fb46b04ac10a15ada87c81b4835f7
ramizzang <rkfkaals@gmail.com>
2024-03-05 16:47:16 +0900

    init commit

---
+++ src/main/java/com/sparta/classapi/ClassApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.classapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/classapi/ClassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.classapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
